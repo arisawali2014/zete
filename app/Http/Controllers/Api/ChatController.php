0f47636dd686e1d001219f0d9dc010fdd3746f52
Muhamad Ari Sawali <arisawali2020@gmail.com>
2023-04-29 14:02:44 +0700

    Fix Chats api

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -35,29 +35,31 @@ class ChatController extends Controller
         if(substr($phone, 0, 1) == '0'){
             $phone = '62' . substr($phone, 1);
         }
-        $curl = curl_init();
-        // new post
-        $data = array(
-            'receiver' => $phone,
-            'message' => $message
-        );
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->base_url . "/chats/send?id=" . $device_id,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($data),
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json"
-            ),
-        ));
-        $result = curl_exec($curl);
-        curl_close($curl);
-        $result = json_decode($result);
+
+        $result = sendMessages($device_id, $phone, $message);
+        // $curl = curl_init();
+        // // new post
+        // $data = array(
+        //     'receiver' => $phone,
+        //     'message' => $message
+        // );
+        // curl_setopt_array($curl, array(
+        //     CURLOPT_URL => $this->base_url . "/chats/send?id=" . $device_id,
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_ENCODING => "",
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 0,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => "POST",
+        //     CURLOPT_POSTFIELDS => json_encode($data),
+        //     CURLOPT_HTTPHEADER => array(
+        //         "Content-Type: application/json"
+        //     ),
+        // ));
+        // $result = curl_exec($curl);
+        // curl_close($curl);
+        // $result = json_decode($result);
 
         if($result->success == true){
             $status = 200;
